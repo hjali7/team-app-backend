368e0619fe5011218e1dd4b1b3d36541870f36f0
hjali7 <hjikingali@gmail.com>
2025-11-12 13:12:25 +0000

    solve CORS

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.24.9
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3
+	github.com/go-chi/cors v1.2.2
 	github.com/go-playground/validator/v10 v10.28.0
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/jackc/pgx/v5 v5.7.6
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,10 +3,16 @@ package server
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) registerRoutes() {
 	// Middlewares
+	s.router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "http://team.looparc.ir"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+	}))
 	s.router.Use(middleware.Logger)
 	s.router.Use(middleware.Recoverer)
 
